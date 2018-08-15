ac1cdfafc0cc7d1d3548c5557ac2d6c9d6d96b7d
Marcus Turnbo <marcus@emarcus.com>
2018-08-14 23:33:35 -0500

    Updated module.  Version 1.0.0

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
+'use strict';
+
 var units = require('./units.json');
 
-function round(value, decimals) {
-  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+/**
+ * Rounds number to specified number of significant digits
+ * @param {number} num
+ * @param {number} digits
+ * @return {number}
+ */
+function round(num, digits) {
+  return Number(Math.round(num+'e'+digits)+'e-'+digits);
 }
 
-module.exports = function convert(measurement, unit, value, precision = 4) {
+/**
+ * Converts value to units in Hot Pockets
+ * @param {string} measurement
+ * @param {string} unit
+ * @param {number} value
+ * @param {number} precision
+ * @return {number}
+ */
+function convert(measurement, unit, value, precision = 4) {
   var result = units[measurement][unit] && value / units[measurement][unit].value;
   return round(result, precision);
-};
+}
+
+
+module.exports = convert;
